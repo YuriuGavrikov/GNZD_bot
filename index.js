d3ab9a4e01fe674abfe6880e48f3462eed8428a4
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2023-07-31 07:54:31 +0300

    fix bot

--- index.js
+++ index.js
@@ -13,17 +13,38 @@ bot.setMyCommands([
 		command: '/whatismyname',
 		description: 'Как меня зовут',
 	},
+	{
+		command: '/timetotalk',
+		description: 'Кто будет рассказывать',
+	},
 ]);
 
 bot.on('message', async (msg) => {
 	const text = msg.text;
 	const chatId = msg.chat.id;
 	const name = msg.from.first_name;
+	const users = [];
+
+	const getUsers = () =>
+		bot.getChatAdministrators(chatId).then((data) => {
+			data.forEach((user) => {
+				if (user.user.is_bot === false) users.push(user.user.first_name);
+			});
+		});
 
 	if (text === '/start' || text === '/start@the_gnzd_bot') {
 		return bot.sendMessage(chatId, `Здарова, ты попал к боту банды GNZD`);
 	}
 	if (text === '/whatismyname' || text === '/whatismyname@the_gnzd_bot') {
 		return bot.sendMessage(chatId, `Тебя зовут ${name}`);
+	}
+	if (text === '/timetotalk' || text === '/timetotalk@the_gnzd_bot') {
+		await getUsers();
+		return bot.sendMessage(
+			chatId,
+			`${
+				users[Math.floor(Math.random() * users.length)]
+			} тебе повезло, записывай кружок`
+		);
 	}
 });
