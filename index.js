98e528383f8cd194b1958c2614ec2be778ed2dae
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2023-08-03 19:23:14 +0300

    finished the daily notification

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const TelegramApi = require('node-telegram-bot-api');
+const cron = require('node-cron');
 
 const TOKEN = '6421481723:AAGecmtKjMac1rE9aRD9F3gfuOJ0hc28bsk';
 
 const bot = new TelegramApi(TOKEN, { polling: true });
+
+let notification = null;
+
+let isNotification = false;
+let currentChatId = '';
+let currentData = [];
+
+const dailyNotification = (chatId, data) => {
+	//Функция ежедневного уведомления
+	const dailyChatId = chatId;
+	const dailyData = data;
+
+	return cron.schedule('00 11 * * *', () => {
+		bot.sendMessage(
+			dailyChatId,
+			`🎉 ${dailyData[Math.floor(Math.random() * dailyData.length)]
+			} тебе повезло 🎉, записывай кружок ⭕`
+		);
+	});
+};
 
 bot.setMyCommands([
 	{
@@ -14,8 +35,12 @@ bot.setMyCommands([
 		description: 'Как меня зовут',
 	},
 	{
-		command: '/timetotalk',
-		description: 'Кто будет рассказывать',
+		command: '/ontimetotalk',
+		description: 'Включить уведомление о кружке',
+	},
+	{
+		command: '/offtimetotalk',
+		description: 'Отключить уведомление о кружке',
 	},
 ]);
 
@@ -25,6 +50,7 @@ bot.on('message', async (msg) => {
 	const name = msg.from.first_name;
 
 	const getUsers = new Promise(function (resolve, reject) {
+		//Получает всех юзеров в чате кроме ботов
 		const users = [];
 		if (/^-/.test(chatId)) {
 			bot.getChatAdministrators(chatId)
@@ -46,23 +72,59 @@ bot.on('message', async (msg) => {
 		return bot.sendMessage(chatId, `Здарова, ты попал к боту банды GNZD`);
 	}
 	if (text === '/whatismyname' || text === '/whatismyname@the_gnzd_bot') {
-		return bot.sendMessage(chatId, `Тебя зовут ${name}`);
+		return bot.sendMessage(chatId, `Тебя зовут ${name} 🎉`);
 	}
-	if (text === '/timetotalk' || text === '/timetotalk@the_gnzd_bot') {
-		getUsers.then((data) => {
-			if (data.length === 0) {
-				return bot.sendMessage(
-					chatId,
-					'Это работает только в групповых чатах'
-				);
-			} else {
-				return bot.sendMessage(
-					chatId,
-					`${
-						data[Math.floor(Math.random() * data.length)]
-					} тебе повезло, записывай кружок`
-				);
+	if (text === '/ontimetotalk' ||
+		text === '/ontimetotalk@the_gnzd_bot' ||
+		text === '/offtimetotalk' ||
+		text === '/offtimetotalk@the_gnzd_bot') {
+		if (/^-/.test(chatId) !== true) {
+			//Проверяет групповой это чат или нет
+			bot.sendMessage(
+				chatId,
+				'Это работает только в груповых чатах'
+			);
+		} else {
+			if (text === '/ontimetotalk' ||
+				text === '/ontimetotalk@the_gnzd_bot') {
+				//Включает ежедневное уведомление
+				if (isNotification === false) {
+					isNotification = true;
+					getUsers.then((data) => {
+						currentChatId = chatId;
+						currentData = data;
+					}).then(() => {
+						notification = dailyNotification(currentChatId, currentData);
+					}).then(() => {
+						notification.start();
+						bot.sendMessage(
+							chatId,
+							'Уведомление включено'
+						);
+					});
+				} else {
+					bot.sendMessage(
+						chatId,
+						'Уведомление уже включено'
+					);
+				}
+			} else if (text === '/offtimetotalk' ||
+				text === '/offtimetotalk@the_gnzd_bot') {
+				//Отключает ежедневное уведомление
+				if (isNotification === true) {
+					isNotification = false;
+					notification.stop();
+					bot.sendMessage(
+						chatId,
+						'Уведомление отключено'
+					);
+				} else {
+					bot.sendMessage(
+						chatId,
+						'Уведомление уже отключено'
+					);
+				}
 			}
-		});
+		}
 	}
 });
